7cc75a277446076deb94c7cc372046cfa04e66d5
ST10478718 <st10478718@vcconnect.edu.za>
2025-09-09 14:35:58 +0200

    Initial commit

---
+++ app/src/main/java/za/co/varsitycollege/st10478718/empoweringthenationsst10494672st10478718/CoursesScreen.kt
@@ -0,0 +1,28 @@
+package za.co.varsitycollege.st10478718.empoweringthenationsst10494672st10478718
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CoursesScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_courses_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnViewCourses = findViewById<Button>(R.id.btnViewCourses)
+
+        btnViewCourses.setOnClickListener{
+            val intent = Intent( this,ShortCourses::class.java)
+            startActivity(intent)
+        }
+    }
+}
